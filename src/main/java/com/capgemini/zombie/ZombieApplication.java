e0126f8337dd64af302312ed61e6e6c747ae2a4f
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-05-11 18:12:48 +0200

    Project Z started

---
+++ src/main/java/com/capgemini/zombie/ZombieApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.zombie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZombieApplication.class, args);
+	}
+}
